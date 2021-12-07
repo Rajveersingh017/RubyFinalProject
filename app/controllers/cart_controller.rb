f64d0c11476c78c55306092555775d8407ef73cd
Rajveer <rajveersingh017@outlook.com>
2021-12-07 05:08:23 -0600

    cart controller is made

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,2 +1,24 @@
 class CartController < ApplicationController
+  def index
+    @songs =  Song.find(session[:shopping_cart] )
+    @total = 0
+  end
+
+  def create
+    logger.debug("Adding #{params[:id]} to cart")
+
+    id = params[:id].to_i
+    session[:shopping_cart] << id # puts onto the array on the end
+    product = Song.find(id)
+    flash[:notice] = "➕ #{product.title} added to cart!"
+    redirect_to root_path
+  end
+
+  def destroy
+    id = params[:id].to_i
+    session[:shopping_cart].delete(id)
+    product = Song.find(id)
+    flash[:notice] = "➖ #{product.title} removed from cart!"
+    redirect_to cart_index_path
+  end
 end
