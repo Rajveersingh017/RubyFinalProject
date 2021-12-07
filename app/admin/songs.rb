0a326057c9405c003694a98cc751bff5885ccfd0
Rajveer <rajveersingh017@outlook.com>
2021-12-07 03:23:45 -0600

    image column is added

--- app/admin/songs.rb
+++ app/admin/songs.rb
@@ -5,7 +5,16 @@ ActiveAdmin.register Song do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-   permit_params :title, :genre, :length, :rating, :instock, :price, :store_id, :album_id
+   permit_params :title, :genre, :length, :rating, :instock, :price, :store_id, :album_id, :image
+
+   form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+    f.inputs do
+        f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image) : ""
+  end
+    f.actions         # adds the 'Submit' and 'Cancel' buttons
+  end
   #
   # or
   #
